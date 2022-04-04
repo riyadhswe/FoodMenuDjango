4b2e88fb7acc8ac7b31d56424e84597d800ebd92
riyadhswe <riyadhswe@gmail.com>
2022-04-05 01:15:07 +0600

    Adding Additional fiels

--- FoodMenuDjango/urls.py
+++ FoodMenuDjango/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from AppUser import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('AppFood.urls')),
+    path('register/', user_views.register, name='register'),
 ]
