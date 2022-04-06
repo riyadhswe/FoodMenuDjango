65e0db0677c16a1801c29897ef8ec82dabdd1950
riyadhswe <riyadhswe@gmail.com>
2022-04-06 23:03:08 +0600

    59. Add User To Post

--- AppFood/views.py
+++ AppFood/views.py
@@ -1,27 +1,41 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .models import *
 from .forms import *
-
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 # Create your views here.
-def index(request):
+"""def index(request):
     item_list = Item.objects.all()
     contex = {
         'item_list': item_list,
     }
     return render(request, 'food/index.html', contex)
+"""
+
+
+class IndexClassView(ListView):
+    model = Item
+    template_name = 'food/index.html'
+    context_object_name = 'item_list'
 
 
 def item(request):
     return HttpResponse('This is an item')
 
 
-def detail(request, item_id):
+"""def detail(request, item_id):
     item = Item.objects.get(pk=item_id)
     context = {
         'item': item,
     }
     return render(request, 'food/detail.html', context)
+"""
+
+
+class FoodDetail(DetailView):
+    model = Item
+    template_name = 'food/detail.html'
 
 
 def add(request):
